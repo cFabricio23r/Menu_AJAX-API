4d0ca562a67f36f8efe31ed3e749f0f84203b5a6
Fabricio Ramirez <fabricio.23@live.com>
2019-06-01 19:15:22 -0600

    first commit

---
+++ scripts.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+
+    $("#btn-get-datos").click(function () {
+        getData();
+    });
+
+    function getData() {
+        $.ajax({
+            url: "https://mini-shop-rg.herokuapp.com/products",
+            type: "GET",
+            dataType: "json",
+            success: function (respuesta) {
+                var divListaMenu = $("#lista-menu");
+                console.log(respuesta.data);
+                var collectionMenu = respuesta.data;
+                var newElement = "";
+                for (var i = 0; i < collectionMenu.length; i++) {
+                    var dato = collectionMenu[i];
+                    newElement +=
+                        "<div class='card mt-4'>" +
+                        "<img class='card-img-top img-fluid' src=" +
+                        dato.image +
+                        " />" +
+                        "<div class='card-body'>" +
+                        "<h3 class='card-title'>" +
+                        dato.name +
+                        "</h3>" +
+                        "<h4>" +
+                        dato.price +
+                        "</h4>" +
+                        "<p class='card-text'>"+
+                        dato.description +
+                        "</p>" +
+                        "<span class='text-warning'>&#9733; &#9733; &#9733; &#9733; &#9734;</span> 4.0 stars" +
+                        "</div></div>"                  
+                };
+                divListaMenu.html(newElement);
+            },
+            error: function (error) {
+                alert("Ha ocurrido un error!");
+            }
+        });
+    }
+});
